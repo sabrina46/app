c2032ff82740aeaae9df123863ed2f2798b0c179
sabrina46 <46074025@qq.com>
2019-09-30 15:27:20 +0800

    login

--- music/src/view/Login/index.js
+++ music/src/view/Login/index.js
@@ -1,11 +1,46 @@
-import React,{PureComponent} from 'react'
-class Login extends PureComponent{
-    render (){
+import React from "react";
+import { Form, Icon, Input, Button, Checkbox } from "antd";
+import { PureComponent } from React;
+class Login extends PureComponent {
+    render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <Layout>
-                <LoginCard>1</LoginCard>
-            </Layout>
-        )
-    }
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="Username"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(<Checkbox>Remember me</Checkbox>)}
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+              Or <a href="">register now!</a>
+            </Form.Item>
+          </Form>
+        );
+      }
 }
-export default Login;
