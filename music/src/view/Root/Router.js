e5f95bf55b3b0796b25c57fcc5545435c1733c06
sabrina46 <46074025@qq.com>
2019-09-30 15:55:05 +0800

    test

--- music/src/view/Root/Router.js
+++ music/src/view/Root/Router.js
@@ -2,13 +2,15 @@ import React from 'react';
 import App from '../../App';
 import Recommend from '../recommendation';
 import Search from '../home/Search'
+import Login from '../Login'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 const BasicRouter = () => (
     <Router>
       <Switch>
-      <Route path="/" exact component={App}></Route>
+      <Route path="/" exact component={Login}></Route>
+      <Route path="/Recommend" component={App}></Route>
       <Route path="/Recommend" component={Recommend}></Route>
       <Route path="/Search" component={Search}></Route>
       </Switch>
